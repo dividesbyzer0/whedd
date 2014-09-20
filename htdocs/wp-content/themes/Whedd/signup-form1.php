a07cd1a16b1677329e28fb91262cb459aa34122e
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-20 17:18:12 -0500

    cont signup form1 setup

--- htdocs/wp-content/themes/Whedd/signup-form1.php
+++ htdocs/wp-content/themes/Whedd/signup-form1.php
@@ -1,6 +1,73 @@
-<div id="form"> 
 <form action="">
-<input type="radio" name="sex" value="male">Male<br>
-<input type="radio" name="sex" value="female">Female
-</form>
+  <fieldset class="radioGroup">
+    <legend>Do you have a wedding date?</legend>
+    <label for="rb-date-no">
+      <input type="radio" name="date-set" value="yes" id="rb-date-yes" />
+      YES
+    </label>
+    <label for="rb-date-no">
+      <input type="radio" name="date-set" value="no" id="rb-date-no" />
+      NO
+    </label>
+  </fieldset>
+  <div class="w-date">
+<p>Date: <input id="fullDate" type="text" style="display:block"></p>
+    <select id="month" name="month">
+        <option value="01">January</option> 
+        <option value="02">February</option>
+        <option value="03">March</option>
+        <option value="04">April</option>
+        <option value="05">May</option>
+        <option value="06">June</option>
+        <option value="07">July</option>
+        <option value="08">August</option>
+        <option value="09">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+    </select>
+    <select id="day" name="day">
+        <option value="01">1</option>
+        <option value="02">2</option>
+        <option value="03">3</option>
+        <option value="04">4</option>
+        <option value="05">5</option>
+        <option value="06">6</option>
+        <option value="07">7</option>
+        <option value="08">8</option>
+        <option value="09">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+        <option value="28">28</option>
+        <option value="29">29</option>
+        <option value="30">30</option>
+        <option value="31">31</option>
+    </select>
+    <select id="year" name="year">
+        <option value="2011">2011</option>
+        <option value="2012">2012</option>
+        <option value="2013">2013</option>
+        <option value="2014">2014</option>
+        <option value="2015">2015</option>
+    </select>
+</div>
+<p>If "No," <a href="#0">skip to our vendor section</a></p>
+<div id="next_button" class="button">
+  <button>Next</button>
 </div>
