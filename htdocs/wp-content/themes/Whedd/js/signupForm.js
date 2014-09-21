e7d89d3a58fd9158bdf6488439d7c14a762a8d4e
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-21 00:36:31 -0500

    reset selector name for error signupForm1

--- htdocs/wp-content/themes/Whedd/js/signupForm.js
+++ htdocs/wp-content/themes/Whedd/js/signupForm.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#signupForm1 #nextButton').click(function(){
-    if ($("input[name='html_elements']:checked").val() == "") {
+    if ($("input[name='dateSet']:checked").val() == "") {
       $('#signupForm1 .error').removeClass('hide');
-      $('#signupForm1 .error').html('<p>Please chose "Yes" or "No" and select a date</p>');
+      $('#signupForm1 .error').html('<p>Please choose "Yes" or "No" and select a date</p>');
       return false;
     } else {
       $('#signupForm1').hide('slide', {direction: 'right'}, 1400); $('#signupForm2').delay(600).show('slide', {direction: 'left'}, 1400);
