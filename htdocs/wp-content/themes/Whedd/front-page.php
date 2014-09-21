aeec9eb2759496a75869b365e3a61a433375f34f
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-20 22:53:28 -0500

    signup form padding adjustments add Logo img

--- htdocs/wp-content/themes/Whedd/front-page.php
+++ htdocs/wp-content/themes/Whedd/front-page.php
@@ -31,7 +31,7 @@ $args = array('redirect' => get_permalink( get_page( $page_id_of_member_area ) )
           </div>
         </div>
         <div id='signup-1' class="loginForm circle center-parent">
-          Form 2
+          <img id="logoForm" title="whedd" src="<?php bloginfo('template_directory'); ?>/images/My-logo-light.png" alt="whedd">
           <div class="center-child">
             <?php get_template_part( 'signup', 'form1'); ?>
           </div>
