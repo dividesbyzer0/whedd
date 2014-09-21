1517253c3eff5c485cd8024724f8aa2f0bd3179a
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-21 01:22:40 -0500

    smooth trasitions on error

--- htdocs/wp-content/themes/Whedd/js/signupForm.js
+++ htdocs/wp-content/themes/Whedd/js/signupForm.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
       $('#signupForm1').hide('slide', {direction: 'right'}, 1400); $('#signupForm2').delay(600).show('slide', {direction: 'left'}, 1400);
       return false;
     } else {
-      $('#signupForm1 .error').removeClass('hide');
       $('#signupForm1 .error').html('<p>Please choose "Yes" or "No" and select a date</p>');
+      $('#signupForm1 .error').fadeIn();
+      $('#signupForm1 #nextButton').css('margin-top', '20%');
       return false;
     }
   })
