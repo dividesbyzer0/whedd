eb5acef0d97dd8e7607a09bd3833f13baf7cea50
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-22 15:17:22 -0500

    adjusted header.js scripts for small device compatibility

--- htdocs/wp-content/themes/Whedd/js/header.js
+++ htdocs/wp-content/themes/Whedd/js/header.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     var height = $(window).scrollTop();
     if(height  > 10) {
       $('header').css('background-color', 'rgba(0, 0, 0, .5');
-      $('header').height('2.0243%');
+      $('header').height('4.0486%');
       $('nav').css('margin-top', '0');
-      $('#logoImg').height('2.0243%');
+      $('#logoImg').height('1.25rem');
     } else {
       $('header').css('background-color', 'transparent');
-      $('header').height('4.0486%');
+      $('header').height('8.097%');
       $('nav').css('margin-top', '1.0121%');
-      $('#logoImg').height('4.0486%');
+      $('#logoImg').height('2.5rem');
     }
   })
 })
