89149a2d13ab522a7bf96a9b756f60d5d50475e8
Jim Erickson <jim.a.erickson@gmail.com>
2014-09-23 00:29:31 -0500

    reconfigured signup slide animation for webkit compliance

--- htdocs/wp-content/themes/Whedd/js/signupForm.js
+++ htdocs/wp-content/themes/Whedd/js/signupForm.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $('#signupForm1 .nextButton').click(function(){
     if ($("input[name='dateSet']:checked").val() ) {
-      $('#signupForm1').hide('slide', {direction: 'right'}, 1400); $('#signupForm2').delay(600).show('slide', {direction: 'left'}, 1400);
+      $('#main').hide('slide', {direction: 'right'}, 1400);
+      $('#signupForm1').hide();
+      $('#signupForm2').show();
+      $('#main').show('slide', {direction: 'left'}, 1400);
+      // $('#signupForm1').hide('slide', {direction: 'right'}, 1400); $('#signupForm2').delay(600).show('slide', {direction: 'left'}, 1400);
       return false;
     } else {
       $('#signupForm1 .error').html('<p>Please choose "Yes" or "No" and select a date</p>');
@@ -11,11 +15,19 @@ $(document).ready(function() {
     }
   })
   $('#signupForm2 .prevButton').click(function(){
-    $('#signupForm2').hide('slide', {direction: 'left'}, 1400); $('#signupForm1').delay(600).show('slide', {direction: 'right'}, 1400);
+    $('#main').hide('slide', {direction: 'right'}, 1400);
+    $('#signupForm2').hide();
+    $('#signupForm1').show();
+    $('#main').show('slide', {direction: 'left'}, 1400);
+    // $('#signupForm2').hide('slide', {direction: 'left'}, 1400); $('#signupForm1').delay(600).show('slide', {direction: 'right'}, 1400);
     return false;
   })
   $('#signupForm2 .nextButton').click(function(){
-    $('#signupForm2').hide('slide', {direction: 'right'}, 1400); $('#signupForm3').delay(600).show('slide', {direction: 'left'}, 1400);
+    $('#main').hide('slide', {direction: 'right'}, 1400);
+    $('#signupForm2').hide();
+    $('#signupForm3').show();
+    $('#main').show('slide', {direction: 'left'}, 1400);
+    // $('#signupForm2').hide('slide', {direction: 'right'}, 1400); $('#signupForm3').delay(600).show('slide', {direction: 'left'}, 1400);
     return false;
   })
 })
