c9d2c6af5dfd214e693390387460c591b6bc5f38
Jim Erickson <jim.a.erickson@gmail.com>
2014-10-18 19:49:18 -0500

    added previously deselected theme files to _s

---
+++ htdocs/wp-content/themes/Whedd/front-page.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Shape
+ * @since Shape 1.0
+ */
+ 
+get_header(); 
+$args = array('redirect' => get_permalink( get_page( $page_id_of_member_area ) ) );
+?>
+
+  <section id="primary"> <!-- main site content-->
+    <div id="main" class="row">
+      <div class="grid col-7">
+        <div id='login' class="loginForm circle center-parent">
+          <img id="logoLoginForm" class="logoImg" title="whedd" src="<?php bloginfo('template_directory'); ?>/images/My-logo-light.png" alt="whedd">
+          <div class="center-child">
+            <?php wp_login_form($args); ?>
+            <?php if(isset($_GET['login']) && $_GET['login'] == 'failed') {?>
+            <div id="login-error" class="error">
+              <p>Login failed: You have entered an incorrect Username or password, please try again.</p>
+            </div>
+            <?php } ?>
+          </div>
+        </div>
+        <div id='signupForm1' class="loginForm circle center-parent">
+          <img id="logoSignupForm1" class="logoImg" title="whedd" src="<?php bloginfo('template_directory'); ?>/images/My-logo-light.png" alt="whedd">
+          <div class="center-child">
+            <?php get_template_part( 'signup', 'form1'); ?>
+          </div>
+        </div>
+        <div id='signupForm2' class="loginForm circle center-parent">
+          <img id="logoSignupForm2" class="logoImg" title="whedd" src="<?php bloginfo('template_directory'); ?>/images/My-logo-light.png" alt="whedd">
+          <div class="center-child">
+            <?php get_template_part( 'signup', 'form2'); ?>
+          </div>
+        </div>
+        <div id='signupForm3' class="loginForm circle center-parent">
+          <img id="logoSignupForm3" class="logoImg" title="whedd" src="<?php bloginfo('template_directory'); ?>/images/My-logo-light.png" alt="whedd">
+          <div class="center-child">
+            <?php get_template_part( 'signup', 'form3'); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php
+    get_sidebar();
+    get_footer();
+  ?>
---
+++ htdocs/wp-content/themes/Whedd/js/signupForm.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('#signupForm1 .nextButton').click(function(){
+    if ($("input[name='dateSet']:checked").val() ) {
+      $('#main').hide('slide', {direction: 'right'}, 1400);
+      $('#signupForm1').delay(900).hide(0);
+      $('#signupForm2').delay(1000).show(0);
+      $('#main').show('slide', {direction: 'left'}, 1400);
+      // $('#signupForm1').hide('slide', {direction: 'right'}, 1400); $('#signupForm2').delay(600).show('slide', {direction: 'left'}, 1400);
+      return false;
+    } else {
+      $('#signupForm1 .error').html('<p>Please choose "Yes" or "No" and select a date</p>');
+      $('#signupForm1 .error').fadeIn();
+      $('#signupForm1 .nextButton').css('margin-top', '20%');
+      return false;
+    }
+  })
+  $('#signupForm2 .prevButton').click(function(){
+    $('#main').hide('slide', {direction: 'left'}, 1400);
+    $('#signupForm2').delay(900).hide(0);
+    $('#signupForm1').delay(1000).show(0);
+    $('#main').show('slide', {direction: 'right'}, 1400);
+    // $('#signupForm2').hide('slide', {direction: 'left'}, 1400); $('#signupForm1').delay(600).show('slide', {direction: 'right'}, 1400);
+    return false;
+  })
+  $('#signupForm2 .nextButton').click(function(){
+    $('#main').hide('slide', {direction: 'right'}, 1400);
+    $('#signupForm2').delay(900).hide(0);
+    $('#signupForm3').delay(1000).show(0);
+    $('#main').show('slide', {direction: 'left'}, 1400);
+    // $('#signupForm2').hide('slide', {direction: 'right'}, 1400); $('#signupForm3').delay(600).show('slide', {direction: 'left'}, 1400);
+    return false;
+  })
+})
