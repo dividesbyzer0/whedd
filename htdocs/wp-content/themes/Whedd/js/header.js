93f7d2fec483b5940c0ef935b7f266e0eb827187
Jim Erickson <jim.a.erickson@gmail.com>
2014-10-20 15:40:11 -0500

    cleaned up header.js further

--- htdocs/wp-content/themes/Whedd/js/header.js
+++ htdocs/wp-content/themes/Whedd/js/header.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   $(window).scroll(function() {
     var height = $(window).scrollTop();
     if(height  > 10) {
-      $('.page-header').css('background-color', 'rgba(0, 0, 0, .5');
-      $('.page-header').height('4.0486%');
+      $('header.page-header').css('background-color', 'rgba(0, 0, 0, .5');
+      $('header.page-header').height('4.0486%');
       $('nav').css('margin-top', '0');
       $('#logoImg').height('1.25rem');
     } else {
-      $('.page-header').css('background-color', 'transparent');
-      $('.page-header').height('8.097%');
+      $('header.page-header').css('background-color', 'transparent');
+      $('header.page-header').height('8.097%');
       $('nav').css('margin-top', '1.0121%');
       $('#logoImg').height('2.5rem');
     }
